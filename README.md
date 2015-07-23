f01244ba51c07de4c90b2bceceb4e26eb793bfbb
cyio <ibeceo@gmail.com>
2015-07-23 22:11:56 +0800

    修改“最新主题”为默认tab

---
+++ README.md
@@ -0,0 +1,8 @@
+原项目：https://git.oschina.net/2010-yanlei/V2EX_chrome_extension.git
+
+#V2EX的chrome插件
+
+##功能
+
+1. 获取最新主题
+2. 获取最热主题
---
+++ src/popup.js
@@ -0,0 +1,33 @@
+
+
+function transfer(timeStamp){
+	var ymd=new Date();
+	ymd.setTime(timeStamp*1000);
+	var month=(ymd.getMonth()+1).toString();
+	var date=ymd.getDate().toString();
+	return month+"-"+date;
+}
+
+function change(htmlobj,current){
+	$(".header li").removeClass("current");
+	$(".header "+current).addClass("current");
+	var json=JSON.parse(htmlobj.responseText);
+	$("body .content").empty();
+	for(var i=0;i<json.length;i++){
+		$("body .content").append("<li><small>"+transfer(json[i].created)+"</small><a target='_blank' href='"+json[i].url+"'>"+json[i].title+"</a>("+json[i].replies+")</li>");
+	}
+//	$("body a").click(function(){
+//		chrome.tabs.create({ url: $(this).attr("href") });
+//	});  
+}
+document.addEventListener('DOMContentLoaded', function () {	       
+   $(".hot").click(function(){
+		htmlobj=$.ajax({url:"https://www.v2ex.com/api/topics/hot.json",dataType:"json",async:false});
+	    change(htmlobj,".hot"); 
+   });   
+   $(".new").click(function(){
+		htmlobj=$.ajax({url:"https://www.v2ex.com/api/topics/latest.json",dataType:"json",async:false});
+	    change(htmlobj,".new"); 
+   });
+	 $(".new").click();   
+});
